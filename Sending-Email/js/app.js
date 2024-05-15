3e588fd651fd3bed93be8915733156ea5b4a5ce2
Mobin Mahzoon <robotgoodrobot@gmail.com>
2024-05-16 00:53:52 +0330

    build: Create file and structure
     creating files and project structure and styles

---
+++ Sending-Email/js/app.js
@@ -0,0 +1,8 @@
+// variables
+const sendBtn = document.querySelector("#sendBtn"),
+  email = document.querySelector("#email"),
+  subject = document.querySelector("#subject"),
+  message = document.querySelector("#message"),
+  resetBtn = document.querySelector("#resetBtn"),
+  form = document.querySelector("#email-form");
+
