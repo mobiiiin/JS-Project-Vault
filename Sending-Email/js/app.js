a2a793cd3b7dd684e2cf0430b66b69b3f5ab1972
Mobin Mahzoon <robotgoodrobot@gmail.com>
2024-05-16 05:25:47 +0330

    build : Add the settings function
    You can put anything in this function or anything related to the settings section

--- Sending-Email/js/app.js
+++ Sending-Email/js/app.js
@@ -1,8 +1,34 @@
-// variables
+// Select in to html
 const sendBtn = document.querySelector("#sendBtn"),
   email = document.querySelector("#email"),
   subject = document.querySelector("#subject"),
   message = document.querySelector("#message"),
   resetBtn = document.querySelector("#resetBtn"),
-  form = document.querySelector("#email-form");
+  form = document.querySelector("#email-form"),
+  loaders = document.querySelector("#loaders"),
+  spinner = document.querySelector("#spinner"),
+  error = document.querySelectorAll(".error")
 
+// Creating a variable in JS
+let m = subject && email && message;
+let sendEmailImg = document.createElement("img");
+
+// this function just for events
+function eventListener() {
+  document.addEventListener("DOMContentLoaded", settings);
+  email.addEventListener("blur", nemidonam);
+  subject.addEventListener("blur", nemidonam);
+  message.addEventListener("blur", nemidonam);
+  sendBtn.addEventListener("click", activeSendBtn);
+  resetBtn.addEventListener("click", activeResetBtn);
+  form.addEventListener("submit", submitForm);
+  // sendBtn.addEventListener("click",submitForm)
+}
+eventListener()
+
+// For project settings, we can put some other things in it
+function settings() {
+  sendBtn.disabled = true;
+  resetBtn.disabled = true
+
+}
