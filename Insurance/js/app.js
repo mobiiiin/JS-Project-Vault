b491b96840c8c49cecdaf74916afe63050b644ad
Mobin Mahzoon <robotgoodrobot@gmail.com>
2024-06-10 05:33:56 +0330

    [fix,feat] : Persian numbers , Solar year
    Correct conversion of English numbers to Farsi .
    and adding the solar year using the Jalali library .

--- Insurance/js/app.js
+++ Insurance/js/app.js
@@ -1,7 +1,7 @@
 // Select
 function selectedInHtml() {
     let select = document.querySelector("#year");
-    return select
+    return select;
 }
 
 // Creator
@@ -16,19 +16,17 @@ CreateOptionElement.prototype.createOption = function () {
     option.value = this.value;
     option.textContent = this.content;
     console.log(option);
-    return option
+    return option;
 }
 
 // This function is for converting English numbers to Farsi
 fixNumbers = function (str,) {
-    let
-        persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
-        arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
-        englishNumbers = [/0/g, /1/g, /2/g, /3/g, /4/g, /5/g, /6/g, /7/g, /8/g, /9/g]
-
+    str = str.toString();
     if (typeof str === 'string') {
-        for (var i = 0; i < 10; i++) {
-            str = str.replace(englishNumbers[i], i).replace(persianNumbers[i], i);
+        let persianNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+        let englishNumbers = [/0/g, /1/g, /2/g, /3/g, /4/g, /5/g, /6/g, /7/g, /8/g, /9/g];
+        for (let i = 0; i < 10; i++) {
+            str = str.replace(englishNumbers[i], persianNumbers[i]);
         }
     }
     return str;
@@ -37,13 +35,13 @@ fixNumbers = function (str,) {
 // This function displays the years of the car 
 // From this year to 20 years ago
 function ShowTheYearOfProductionOfCars() {
-    let currentYear = new Date().getFullYear();
+    // Solar year
+    let currentYear = moment().jYear();
     let startYear = currentYear - 20;
-    for (currentYear; currentYear >= startYear; currentYear--) {
-        currentYear = fixNumbers(currentYear.toString())
-        let options = new CreateOptionElement("year", currentYear)
-        selectedInHtml().append(options.createOption())
-        
+    for (let year = currentYear; year >= startYear; year--) {
+        let yearInPersian = fixNumbers(year);
+        let options = new CreateOptionElement("year", yearInPersian)
+        selectedInHtml().append(options.createOption());
     }
 }
 ShowTheYearOfProductionOfCars()
