4e10b197fdc660aaeb20c9e1d1eab1948fce712e
Mobin Mahzoon <robotgoodrobot@gmail.com>
2024-12-10 16:28:25 +0330

    [build , refactor] :  Complete the project
    Complete the project and add it to the main project
    Added things: Sidebar

--- Ostadkar/src/js/app.js
+++ Ostadkar/src/js/app.js
@@ -1,5 +1,36 @@
+// selected
+const buttonHumbuggeryMenu = document.querySelector(".container-button-sidebar");
+const sidebar = document.querySelector(".sidebar");
+const move = document.querySelector(".move");
+const li = document.querySelector(".sidebar ul li");
+
+
+// this function just for addeventlistener
+function eventListener() {
+  buttonHumbuggeryMenu.addEventListener("click", buttonSidebar);
+}
+eventListener()
+
+
+// this function for open the sidebar 
+function buttonSidebar() {
+    sidebar.classList.toggle("visible");
+    placeMenu()
+}
+
+// this function for place humbuggery menu
+function placeMenu() {
+    if (sidebar.classList.contains("visible")) {
+        move.style.transition = " margin 0.4s ease";
+        move.style.margin = "0 244px 0 0 "
+    }
+    else{
+      move.style.margin = "0 0 0 0 "
+    }
+}
+
+// this obj for option slider
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'horizontal',
     slidesPerView: 3,  
     slideshow:1,
