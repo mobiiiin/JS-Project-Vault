5d69cd7bd08597f84d4cfca726054b175c41e970
Mobin Mahzoon <robotgoodrobot@gmail.com>
2024-12-10 17:24:04 +0330

    style : commenting

--- Ostadkar/src/js/app.js
+++ Ostadkar/src/js/app.js
@@ -4,51 +4,43 @@ const sidebar = document.querySelector(".sidebar");
 const move = document.querySelector(".move");
 const li = document.querySelector(".sidebar ul li");
 
-
 // this function just for addeventlistener
 function eventListener() {
   buttonHumbuggeryMenu.addEventListener("click", buttonSidebar);
 }
 eventListener()
 
-
 // this function for open the sidebar 
 function buttonSidebar() {
-    sidebar.classList.toggle("visible");
-    placeMenu()
+  sidebar.classList.toggle("visible");
+  placeMenu()
 }
 
 // this function for place humbuggery menu
 function placeMenu() {
-    if (sidebar.classList.contains("visible")) {
-        move.style.transition = " margin 0.4s ease";
-        move.style.margin = "0 244px 0 0 "
-    }
-    else{
-      move.style.margin = "0 0 0 0 "
-    }
+  if (sidebar.classList.contains("visible")) {
+    move.style.transition = " margin 0.4s ease";
+    move.style.margin = "0 244px 0 0 "
+  }
+  else {
+    move.style.margin = "0 0 0 0 "
+  }
 }
 
 // this obj for option slider
 const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    slidesPerView: 3,  
-    slideshow:1,
-    loop: Infinity,
-    
-  
-    // pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      
-    },
-  
-    // Navigation arrows
-    navigation: {
-      prevEl: '.swiper-button-next',
-      nextEl: '.swiper-button-prev',
-    },
-    
-
-  });
+  direction: 'horizontal',
+  slidesPerView: 3,
+  slideshow: 1,
+  loop: Infinity,
+  // pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // Navigation arrows
+  navigation: {
+    prevEl: '.swiper-button-next',
+    nextEl: '.swiper-button-prev',
+  },
+});
